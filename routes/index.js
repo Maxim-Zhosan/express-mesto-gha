8861283865b95d00cb00c2c791950ad5d8d04644
Maxim Zhosan <maxim.zhosan@gmail.com>
2022-12-12 19:07:30 +0300

    Feature: PW14 - 2

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const index = require('express').Router();
 
-// const pageNotFoundError = (req, res) => {
-//   res.status(404).send({ message: 'Страница не найдена' });
-// };
+const pageNotFoundError = (req, res) => {
+  res.status(404).send({ message: 'Страница не найдена' });
+};
 
 index.use('/users', require('./users'));
 index.use('/cards', require('./cards'));
 
-// index.use('/', pageNotFoundError);
+index.use('/', pageNotFoundError);
 
 module.exports = index;
--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,9 @@ const {
 
 users.post('/signup', celebrate({
   body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().uri(),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(6),
   }).unknown(true),
