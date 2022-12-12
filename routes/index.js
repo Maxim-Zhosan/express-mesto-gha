eb535ec97706ef2fd01fde9f385deabcf6222195
Maxim Zhosan <maxim.zhosan@gmail.com>
2022-12-12 19:14:08 +0300

    Feature: PW14 - 3

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,8 @@
 const index = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const {
+  createUser, login,
+} = require('../controllers/users');
 
 const pageNotFoundError = (req, res) => {
   res.status(404).send({ message: 'Страница не найдена' });
@@ -6,6 +10,22 @@ const pageNotFoundError = (req, res) => {
 
 index.use('/users', require('./users'));
 index.use('/cards', require('./cards'));
+
+index.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().uri(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6),
+  }).unknown(true),
+}), createUser);
+index.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6),
+  }),
+}), login);
 
 index.use('/', pageNotFoundError);
 
--- routes/users.js
+++ routes/users.js
@@ -2,24 +2,9 @@ const users = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 const auth = require('../middlewares/auth');
 const {
-  getUsers, getUserById, createUser, updateProfile, updateAvatar, login, getCurrentUser,
+  getUsers, getUserById, updateProfile, updateAvatar, getCurrentUser,
 } = require('../controllers/users');
 
-users.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().uri(),
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(6),
-  }).unknown(true),
-}), createUser);
-users.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(6),
-  }),
-}), login);
 users.get('/', auth, getUsers);
 users.get('/me', auth, getCurrentUser);
 users.patch('/me', auth, celebrate({
